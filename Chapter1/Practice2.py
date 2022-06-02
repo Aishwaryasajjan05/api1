409b92102a0a4b18121c55db95ef4a7fe277de31
Aishwaryasajjan05 <aishwaryaveerendar@gmail.com>
2022-06-02 11:31:22 +0530

    Books

---
+++ Chapter1/Practice2.py
@@ -0,0 +1,20 @@
+from typing import Optional
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+
+BOOKS = {
+    'book_1': {'title': 'Title One', 'author': 'Author One'},
+    'book_2': {'title': 'Title Two', 'author': 'Author Two'},
+    'book_3': {'title': 'Title Three', 'author': 'Author Three'},
+    'book_4': {'title': 'Title Four', 'author': 'Author Four'},
+    'book_5': {'title': 'Title Five', 'author': 'Author Five'},
+}
+
+
+@app.get("/{book_name}")
+async def read_book(book_name: str):
+    return BOOKS[book_name]
+    
